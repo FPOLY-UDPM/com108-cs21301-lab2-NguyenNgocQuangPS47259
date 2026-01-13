00cd7d627c310ac5f519f424e39f3d7408df90dd
Tên của bạn <quangnguyendeptrai1711@gmail.com>
2026-01-13 10:00:13 +0700

    nộp bài 5

--- bai5.c
+++ bai5.c
@@ -10,10 +10,19 @@
 #include <stdio.h>
 
 int main(){
-    int so1, so2;
-
-    printf("Chương trình tính tích và thương của 2 số\n");
-
-
+    float so1=4, so2=2,tich=3;
+    printf("hãy nhập giá trị so1 :");
+    scanf("%f",&so1);
+    printf("hãy nhập giá trị so2 :");
+    scanf("%f",&so2);
+    tich=so1*so2;
+    printf("tích số của số 1 và 2 là:%.2f\n",tich);
+    if (so2==0)
+    {
+        printf("phương trình vô nghiệm");
+    }else
+    {
+        printf("thương số của số 1 và 2 là : %.2f\n",so1/so2);
+    }
     return 0;
 } 
